b0573fe7a24f1647eea6efbc1462443ef7368b56
githubninpoka <github@ninpoka.nl>
2024-11-09 16:50:25 +0100

    Add project files.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models;
+
+public class Server
+{
+    public Server()
+    {
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(0, 2);
+        IsOnline = randomNumber == 0 ? false : true;
+    }
+    public int ServerId { get; set; }
+    public bool IsOnline { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? City { get; set; } // setting 'required' before 'string' does NOT make it required in the edit form.
+    // at least the <DataAnnotationsValidator> in the form does not seem to be triggered
+}
